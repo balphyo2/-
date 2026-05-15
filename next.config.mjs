c5c46187b739b00e767c05a1b47add4bb91a54f7
balphyo2 <2024000119@unam.hs.kr>
2026-05-15 16:43:57 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export', // 이 줄을 추가해줍니다!
   images: {
-    unoptimized: true,
+    unoptimized: true, // 깃허브 페이지에서 이미지 잘 나오게 하는 설정
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
